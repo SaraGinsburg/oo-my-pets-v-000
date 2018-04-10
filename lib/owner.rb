ab00de433fb2af89e3e3501b56b674f1e36020da
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-10 19:14:19 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -40,17 +40,15 @@ class Owner
 
   def buy_dog(name)
     @pets[:dogs] << Dog.new(name)
-     
+
   end
 
   def buy_cat(name)
-    cat = Cat.new(name)
-    @pets[:cats] << cat
+    @pets[:cats] <<  Cat.new(name)
   end
 
   def buy_fish(name)
-    fish = Fish.new(name)
-    @pets[:fishes] << fish
+    @pets[:fishes] <<  Fish.new(name)
   end
 
   def play_with_cats
